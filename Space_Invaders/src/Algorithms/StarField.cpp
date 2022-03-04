19372082548908a81942b8f7f80ef33be97ec038
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2022-03-04 14:27:35 +0800

    Updated star field to use scalable triangles

--- Space_Invaders/src/Algorithms/StarField.cpp
+++ Space_Invaders/src/Algorithms/StarField.cpp
@@ -3,7 +3,7 @@
 
 StarField::StarField(sf::RenderWindow* window, const sf::Sprite* sprite, int star_num):m_window(window),p_sprite(sprite) {
     for (int i = 0; i < star_layc; i++) {
-        star_layers[i] = new sf::VertexArray(sf::Points, star_num);
+        star_layers[i] = new sf::VertexArray(sf::Triangles, star_num*3);
         addStars(star_layers[i]);
     }
 }
@@ -17,10 +17,25 @@ void StarField::addStars(sf::VertexArray* stars) {
 	std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
 	std::uniform_int_distribution<int> rx(-0.25f*m_window->getSize().x,m_window->getSize().x*4); // generation bounds for star field
 	std::uniform_int_distribution<int> ry(-0.25f*m_window->getSize().y,m_window->getSize().y*4); // 
+    std::uniform_real_distribution<float> rs(0.5f,1.0f);    // generation bounds for star size
 
-    // randomly distribute player positions
+    // randomly distribute star positions
+    float star_size;
     for (int i = 0; i < stars->getVertexCount();i++) {
-        (*stars)[i].position = sf::Vector2f(rx(rng), ry(rng));
+        switch (i%3) {
+            case 0:     //triangle vertex 1
+                (*stars)[i].position = sf::Vector2f(rx(rng), ry(rng));
+                star_size = rs(rng);
+                break;
+            case 1:     //triangle vertex 2
+                (*stars)[i].position = (*stars)[i-1].position + sf::Vector2f(star_size, 1.8f*star_size);
+                break;
+            case 2:     //triangle vertex 3
+                (*stars)[i].position = (*stars)[i-2].position + sf::Vector2f(-star_size, 1.8f*star_size);
+                break;
+            default:
+                break;
+        }
     }
 }
 
